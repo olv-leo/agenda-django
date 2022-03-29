350b5a151441240052155b2ac87652e1b3c1649c
olv-leo <1996olv.leo@gmail.com>
2022-03-29 12:09:01 -0300

    Adicionado a paginação na agenda e a opção de editar campos sem entrar no contato na área administrativa, além de um campo mostrar e um campo de pesquisa usando filtros.

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,12 +3,12 @@ from .models import Contato, Categoria
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome')
     list_filter = ('categoria',)
     list_per_page = 5
     search_fields = ('nome', 'email')
-
+    list_editable = ('mostrar',)
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
