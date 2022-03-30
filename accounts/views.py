7d0c0921dd81bd4dff2bc1984811e329221a48eb
olv-leo <1996olv.leo@gmail.com>
2022-03-30 11:12:27 -0300

    Criando formulário para cadastro de novos contatos na área de dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import FormContato
 
 
 def login(request):
@@ -75,4 +76,17 @@ def register(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = FormContato(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar o formulário.')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form.save()
+    messages.success(request, 'Formulário enviado com sucesso!')
+    return render(request, 'accounts/dashboard.html', {'form': FormContato()})
